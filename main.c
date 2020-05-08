ed6fc8cb9454a49c71033ea4193aeb8b48db3d3b
lee <yoonjin671@protonmail.ch>
2020-05-08 19:24:45 +0900

    LIST

---
+++ main.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include"list.h"
+#define SIZE 1000
+int main()
+{
+    list_int intlist=listgen(1000);
+    int i;
+    for(i=1;i<SIZE;i++)
+        intlist.list[i]=i;
+    intlist=listinter(intlist);
+    list_show_all(intlist);
+    return 0;
+}
