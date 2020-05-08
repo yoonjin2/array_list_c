6bc9c101fc4d5ea1b3639c5663faac909f47a8fc
lee <yoonjin671@protonmail.ch>
2020-05-08 22:58:31 +0900

    LIST

--- main_allinone.c
+++ main_allinone.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 struct list_int
@@ -10,15 +11,16 @@ typedef struct list_int list_int;
 list_int list_add(list_int X)
 {
     int i,a,b;
-    scanf("%d %d", &a, &b);
+    scanf("%d", &a);
     if(X.list_size<a)
         ;
     else
     {
         X.list=(int *)realloc(X.list,sizeof(int)*(X.list_size+1));
-        for(i=X.list_size;i>a;i--)
-            X.list[i]=X.list[i-1];
-        X.list_size++;
+        a=X.list_size-a;
+        for(i=a;i<X.list_size-2;i++)
+            X.list[i]=X.list[i+1];
+        scanf("%d",&b);
         X.list[a]=b;
     }
     return X;
@@ -40,14 +42,14 @@ list_int listgen(int size)
     X.list_size=size;
     return X;
 }
-list_int push(list_int X)
+list_int push_inter(list_int X)
 {
     X.list=(int *)realloc(X.list,(X.list_size+1)*sizeof(int));
     scanf("%d",X.list+X.list_size);
     X.list_size++;
     return X;
 }
-list_int pop(list_int X)
+list_int pop_inter(list_int X)
 {
     if(X.list_size==0)
         puts("-1");
@@ -73,14 +75,14 @@ void empty(list_int X)
     else
         puts("0");
 }
-void top(list_int X)
+void top_inter(list_int X)
 {
     if(X.list_size>0)
         printf("%d\n",X.list[X.list_size-1]);
     else
         puts("-1");
 }
-list_int change(list_int X)
+list_int change_inter(list_int X)
 {
     int x,y,sv;
     scanf("%d %d", &x, &y);
@@ -89,29 +91,29 @@ list_int change(list_int X)
     X.list[y]=sv;
     return X;
 }
-void where(list_int X)
+void where_inter(list_int X)
 {
     int x,i;
-    scanf("%d",&x);
+    scanf("%d",x);
     for(i=X.list_size-1;i>=0;i--)
     {
         if(x==X.list[i])
-            printf("%d\n",i);
+            break;
     }
 
 }
 void list_show_all(list_int X)
 {
     int i;
-    for(i=0;i<X.list_size;i++)
+    for(i=X.list_size-1;i>=0;i--)
          printf("%d\n",X.list[i]);
 }
-list_int list_remove(list_int X)
+list_int list_remove_inter(list_int X)
 {
     int Y;
     scanf("%d",&Y);
     for(;Y<X.list_size-1;Y++)
-         X.list[Y+1]=X.list[Y];
+         X.list[Y]=X.list[Y+1];
     X.list_size--;
     X.list=realloc(X.list,sizeof(int)*X.list_size);
     return X;
@@ -125,39 +127,37 @@ list_int listinter(list_int X)
     {
         scanf("%s",command);
         if(strcmp(command,"push")==0)
-            Y=push(Y);
+            Y=push_inter(Y);
         else if(strcmp(command,"pop")==0)
-            Y=pop(Y);
+            Y=pop_inter(Y);
         else if (strcmp(command,"size")==0)
             size(Y);
         else if (strcmp(command,"empty")==0)
             empty(Y);
         else if(strcmp(command,"top")==0)
-            top(Y);
+            top_inter(Y);
         else if(strcmp(command,"change")==0)
-            Y=change(Y);
+            Y=change_inter(Y);
         else if(strcmp(command,"where")==0)
-            where(Y);
+            where_inter(Y);
         else if(strcmp(command,"return")==0)
             break;
         else if(strcmp(command,"showall")==0)
             list_show_all(Y);
         else if(strcmp(command,"remove")==0)
-            Y=list_remove(Y);
+            Y=list_remove_inter(Y);
         else if(strcmp(command,"add")==0)
             Y=list_add(Y);
-        else if(strcmp(command,"clear")==0)
-        {
-            for(i=0;i<Y.list_size;i++)
-                Y.list[i]=0;
-            Y.list_size=0;
-            Y.list=(int *)malloc(sizeof(int));
-        }
         else
             puts("ERROR");
     }
     X.list=realloc(X.list,sizeof(int)*Y.list_size);
     return Y;
 }
-
-
+int main()
+{
+    list_int intlist=listgen(10);
+    intlist=listinter(intlist);
+    list_show_all(intlist);
+    return 0;
+}
