936ad3af88dc759ab1ef6cfcbef658fef2f4a644
yoonjin2 <yoonjin67@gmail.com>
2020-05-09 23:19:47 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,7 @@
-#include<stdio.h>
 #include"list.h"
-#define SIZE 1000
 int main()
 {
-    list_int intlist=listgen(1000);
-    int i;
-    for(i=1;i<SIZE;i++)
-        intlist.list[i]=i;
+    list_int intlist=listgen(1);
     intlist=listinter(intlist);
     list_show_all(intlist);
     free(intlist.list);
